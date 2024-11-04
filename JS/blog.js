6c4b22aa8b0ed4300edc60748c4d7f1b137fa943
imtiajsultan1 <imtiajsultan1@gmail.com>
2024-11-05 00:30:07 +0600

    final

---
+++ JS/blog.js
@@ -0,0 +1,1 @@
+// offWhite('footerID2');
--- JS/home.js
+++ JS/home.js
@@ -1,6 +1,6 @@
 lightGreenColor('btn-blog');
 lightGrayColor('balance-text');
-offWhite('nav-bar');
+// offWhite('nav-bar');
 lightGrayColor10('container-1');
 lightGrayColor10('container-2');
 lightGrayColor10('container-3');
@@ -32,19 +32,52 @@ mainBtnStyle('main-btn-1');
 offWhite('footerID');
 
 
+const historyAdd = document.getElementById('history-list');
+
+const notification1 = document.getElementById('notification-1').innerText;
+const notification2 = document.getElementById('notification-2').innerText;
+const notification3 = document.getElementById('notification-3').innerText;
+
+
+// donate button 1
 document.getElementById('btn-1').addEventListener('click',function(){
     const returnedValue = parseFloat(getInputValue('input-1'));
     let innerTextValueOfMyBalance =  parseFloat(document.getElementById('my-balance').innerText);
     if(returnedValue > 0 && innerTextValueOfMyBalance >= returnedValue){
         let innerTextValue =  parseFloat(document.getElementById('d-balance-1').innerText);
+
+         // start creating ...
+        const newElement = document.createElement('div');
+        newElement.classList.add('p-8', 'border', 'border-gray-300', 'rounded-xl');
+        
+        const newText = document.createElement('h1');
+        newText.classList.add('font-bold', 'text-xl');
+
+        newText.innerText = returnedValue + ' Taka is Donated ' + notification1;
+
+        newElement.appendChild(newText);
+
+        const newParagraph = document.createElement('p');
+        newParagraph.classList.add('text-gray-500');
+        newParagraph.textContent = `Date : ${new Date().toString()}`;
+        
+        newElement.appendChild(newParagraph);
+
+        historyAdd.insertBefore(newElement, historyAdd.firstChild);
+        // end creating element
+
         innerTextValue += returnedValue;
         document.getElementById('d-balance-1').innerText = innerTextValue;
-        
+
         innerTextValueOfMyBalance -= parseFloat(returnedValue);
         document.getElementById('my-balance').innerText = innerTextValueOfMyBalance;
 
         let getModal = document.getElementById('modal-container');
         getModal.classList.remove('hidden');
+
+        document.getElementById('input-1').value = '';
+
+
 
     }
     else{
@@ -52,11 +85,34 @@ document.getElementById('btn-1').addEventListener('click',function(){
     }
 })
 
+// donate button 2
 document.getElementById('btn-2').addEventListener('click',function(){
     const returnedValue = parseFloat(getInputValue('input-2'));
     let innerTextValueOfMyBalance =  parseFloat(document.getElementById('my-balance').innerText);
     if(returnedValue > 0 && innerTextValueOfMyBalance >= returnedValue){
         let innerTextValue =  parseFloat(document.getElementById('d-balance-2').innerText);
+
+        // .....
+        const newElement = document.createElement('div');
+        newElement.classList.add('p-8', 'border', 'border-gray-300', 'rounded-xl');
+        
+        const newText = document.createElement('h1');
+        newText.classList.add('font-bold', 'text-xl');
+
+        newText.innerText = returnedValue + ' Taka is Donated ' + notification2;
+
+        newElement.appendChild(newText);
+
+        const newParagraph = document.createElement('p');
+        newParagraph.classList.add('text-gray-500');
+        newParagraph.textContent = `Date : ${new Date().toString()}`;
+        
+        newElement.appendChild(newParagraph);
+
+        historyAdd.insertBefore(newElement, historyAdd.firstChild);
+        // ....
+
+
         innerTextValue += returnedValue;
         document.getElementById('d-balance-2').innerText = innerTextValue;
         
@@ -65,17 +121,39 @@ document.getElementById('btn-2').addEventListener('click',function(){
 
         let getModal = document.getElementById('modal-container');
         getModal.classList.remove('hidden');
+        document.getElementById('input-2').value = '';
     }
     else{
         alert('Invalid Donation Amount');
     }
 })
 
+// donate button 3
 document.getElementById('btn-3').addEventListener('click',function(){
     const returnedValue = parseFloat(getInputValue('input-3'));
     let innerTextValueOfMyBalance =  parseFloat(document.getElementById('my-balance').innerText);
     if(returnedValue > 0 && innerTextValueOfMyBalance >= returnedValue){
         let innerTextValue =  parseFloat(document.getElementById('d-balance-3').innerText);
+
+        // .....
+        const newElement = document.createElement('div');
+        newElement.classList.add('p-8', 'border', 'border-gray-300', 'rounded-xl');
+        
+        const newText = document.createElement('h1');
+        newText.classList.add('font-bold', 'text-xl');
+
+        newText.innerText = returnedValue + ' Taka is Donated ' + notification3;
+
+        newElement.appendChild(newText);
+
+        const newParagraph = document.createElement('p');
+        newParagraph.classList.add('text-gray-500');
+        newParagraph.textContent = `Date : ${new Date().toString()}`;
+        
+        newElement.appendChild(newParagraph);
+
+        historyAdd.insertBefore(newElement, historyAdd.firstChild);
+        // ....
         innerTextValue += returnedValue;
         document.getElementById('d-balance-3').innerText = innerTextValue;
         
@@ -84,6 +162,8 @@ document.getElementById('btn-3').addEventListener('click',function(){
 
         let getModal = document.getElementById('modal-container');
         getModal.classList.remove('hidden');
+
+        document.getElementById('input-3').value = '';
     }
     else{
         alert('Invalid Donation Amount');
@@ -95,3 +175,26 @@ document.getElementById('close-modal').addEventListener('click',function(){
     let getModal = document.getElementById('modal-container');
         getModal.classList.add('hidden');
 })
+
+const buttonColorChange = document.getElementById('main-btn-2');
+const buttonColorChange2 = document.getElementById('main-btn-1');
+
+document.getElementById('main-btn-2').addEventListener('click', function(){
+        document.getElementById('main-container').classList.add('hidden');
+        document.getElementById('history-list').classList.remove('hidden');
+        
+        buttonColorChange.style.backgroundColor= '#B4F461';
+        
+        buttonColorChange2.style.backgroundColor = '';
+})
+
+document.getElementById('main-btn-1').addEventListener('click', function(){
+        document.getElementById('main-container').classList.remove('hidden');
+        document.getElementById('history-list').classList.add('hidden');
+        
+        buttonColorChange.style.backgroundColor= '';
+        
+        buttonColorChange2.style.backgroundColor = '#B4F461';
+})
+
+
